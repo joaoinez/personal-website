ab149a504312dfbe554ce90b9ce94f318b02a124
João Inez <joaoinez.info@gmail.com>
2024-04-16 19:16:59 +0200

    Update windows script

--- windows.ps1
+++ windows.ps1
@@ -1,5 +1,10 @@
-winget install --id=Microsoft.PowerShell
-winget install --id=Git.Git
-winget install --id=twpayne.chezmoi
+Write-Output "Drawing .slivers/windows"
 
-chezmoi init --apply joaoinez
+winget install --id Microsoft.Powershell
+winget install --id Git.Git
+winget install --id twpayne.chezmoi
+
+Write-Output "Setup completed."
+
+Write-Output "Open a new shell and initialize chezmoi by running:"
+Write-Output "chezmoi init --apply joaoinez"
