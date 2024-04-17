74505ceaf31cd9b26b627d4267daff147c8f7dff
João Inez <joaoinez.info@gmail.com>
2024-04-17 23:43:13 +0200

    Update macos script

--- macos.sh
+++ macos.sh
@@ -3,18 +3,10 @@ set -euo pipefail
 
 echo "Drawing .slivers/macos"
 
-echo "Installing git"
-
-git --version
-
-echo "Installing homebrew"
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 echo "Installing chezmoi"
 
 brew install chezmoi
 
-echo "Initializing chezmoi"
+# echo "Initializing chezmoi"
 
-chezmoi init --apply joaoinez
+# chezmoi init --apply joaoinez
